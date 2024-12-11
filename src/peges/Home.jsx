36a6cd493fea27e1f2b90ee1de83dd261e93003c
ShadWeb <shyifou@gmail.com>
2024-12-11 21:49:16 +0330

    Fix Home

--- src/peges/Home.jsx
+++ src/peges/Home.jsx
@@ -1,9 +1,26 @@
 import HeadHome from "../components/HeadHome"
-
+import serchicon from "../assets/serch.svg"
+import HistoryList from "../components/HistoryList"
 function Home() {
   return (
     <main className="py-6">
       <HeadHome/>
+      <div className="p-4 flex items-center gap-4">  
+        <h2 className="text-2xl font-semibold  mb-4">History</h2>  
+        <div className="flex items-center  py-2 px-6 px  border border-gray-300 rounded-full ">  
+          <input  
+          
+            type="text"  
+            placeholder="Search ..."  
+            className="outline-none w-full"  
+          />  
+        
+        <img src={serchicon} alt="" />
+        </div>  
+  </div>
+
+        <HistoryList/>
+
     </main>
   )
 }
